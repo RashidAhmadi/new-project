e1c4ae4d9f8144a6f0ac4a5454827a7be2cca0d7
Rashid Ahmadi <95165453+RashidAhmadi@users.noreply.github.com>
2025-01-21 22:48:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let modelcar1="Corola";
+let brandcar1="Toyota";
+let yearcar1="2005";
+
+let car1={
+    model: "Corola",
+    brand: "Toyota",
+    year: "2005"
+}
+console.log("this car brand is "+car1.brand);
+let modelcar2="A1";
+let brandcar2="Audi";
+let yearcar2="2010";
+
+let modelcar3="S25";
+let brandcar3="BMW";
+let yearcar3="2015";
+
+  
